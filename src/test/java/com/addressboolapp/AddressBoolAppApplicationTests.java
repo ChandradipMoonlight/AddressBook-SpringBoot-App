ae133e9b030e8f9e1a686ed156622584243f8c50
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-08-14 08:49:19 +0530

    Project setUP is created

---
+++ src/test/java/com/addressboolapp/AddressBoolAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.addressboolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBoolAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
